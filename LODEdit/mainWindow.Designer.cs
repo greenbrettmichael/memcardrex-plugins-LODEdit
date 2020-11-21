73a71c754a6d2c3651a1601c748c9e2caf2e9737
greenbrettmichael <personal@brettmichaelgreen.com>
2020-11-21 17:08:25 -0500

    commit project start with gold

---
+++ LODEdit/mainWindow.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace LODEdit
+{
+    partial class mainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.okButton = new System.Windows.Forms.Button();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.goldNumeric = new System.Windows.Forms.NumericUpDown();
+            this.goldLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.goldNumeric)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // okButton
+            // 
+            this.okButton.Location = new System.Drawing.Point(5, 35);
+            this.okButton.Name = "okButton";
+            this.okButton.Size = new System.Drawing.Size(75, 23);
+            this.okButton.TabIndex = 99;
+            this.okButton.Text = "OK";
+            this.okButton.UseVisualStyleBackColor = true;
+            this.okButton.Click += new System.EventHandler(this.okButton_Click);
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Location = new System.Drawing.Point(87, 35);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(75, 23);
+            this.cancelButton.TabIndex = 0;
+            this.cancelButton.Text = "Cancel";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            this.cancelButton.Click += new System.EventHandler(this.cancelButton_Click);
+            // 
+            // goldNumeric
+            // 
+            this.goldNumeric.Location = new System.Drawing.Point(86, 7);
+            this.goldNumeric.Maximum = new decimal(new int[] {
+            65535,
+            0,
+            0,
+            0});
+            this.goldNumeric.Name = "goldNumeric";
+            this.goldNumeric.Size = new System.Drawing.Size(76, 20);
+            this.goldNumeric.TabIndex = 9;
+            // 
+            // goldLabel
+            // 
+            this.goldLabel.Location = new System.Drawing.Point(12, 9);
+            this.goldLabel.Name = "goldLabel";
+            this.goldLabel.Size = new System.Drawing.Size(68, 13);
+            this.goldLabel.TabIndex = 13;
+            this.goldLabel.Text = "Gold:";
+            this.goldLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // mainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(178, 65);
+            this.Controls.Add(this.goldLabel);
+            this.Controls.Add(this.goldNumeric);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.okButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "mainWindow";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "mainWindow";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.mainWindow_Paint);
+            ((System.ComponentModel.ISupportInitialize)(this.goldNumeric)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button okButton;
+        private System.Windows.Forms.Button cancelButton;
+        private System.Windows.Forms.NumericUpDown goldNumeric;
+        private System.Windows.Forms.Label goldLabel;
+    }
+}
---
+++ LODEdit/mainWindow.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace LODEdit
+{
+    public partial class mainWindow : Form
+    {
+        //Flag for the return value
+        private bool confirmedFlag = false;
+        
+        //A flag for the first run
+        private bool firstRun = true;
+
+        //Currently edited slot
+        private int currentSaveSlot = 0;
+
+        //A data to edit
+        private byte[] saveData = null;
+
+        public mainWindow()
+        {
+            InitializeComponent();
+        }
+
+        //Set up dialog and fetch data
+        public byte[] initDialog(string windowTitle, byte[] gameSaveData)
+        {
+            this.Text = windowTitle;
+            saveData = gameSaveData;
+            loadData(currentSaveSlot);
+
+            //Init default values
+            firstRun = true;
+
+            this.ShowDialog();
+
+            //Check what kind of data should be returned to host application (MemcardRex)
+            if (confirmedFlag == true) return saveData; else return null;
+        }
+
+        //Update slot (save old data, load new data)
+        private void updateSlot(int slotValue)
+        {
+            //Save old data
+            if (firstRun == false) updateData(currentSaveSlot);
+
+            //Update current slot
+            currentSaveSlot = slotValue;
+
+            //Load new data
+            loadData(currentSaveSlot);
+
+            //This is no longer first run
+            firstRun = false;
+        }
+
+        //Load data from the save
+        private void loadData(int slotNumber)
+        {
+            //Load gold
+            goldNumeric.Value = saveData[0x0314 + (slotNumber * 0x600)] | saveData[0x0314 + (slotNumber * 0x600) + 1]<<8;
+        }
+
+        //Place data in the save
+        private void updateData(int slotNumber)
+        {
+            //Store gems in little-endian byte order (16 bit)
+            saveData[0x0314 + (slotNumber * 0x600)] = (byte)((uint)goldNumeric.Value & 0xFF);
+            saveData[0x0314 + (slotNumber * 0x600) + 1] = (byte)(((uint)goldNumeric.Value & 0xFF00)>>8);
+            saveData[0x021C + (slotNumber * 0x600)] = (byte)((uint)goldNumeric.Value & 0xFF);
+            saveData[0x021C + (slotNumber * 0x600) + 1] = (byte)(((uint)goldNumeric.Value & 0xFF00) >> 8);
+
+            calcChecksum(slotNumber);
+        }
+
+        //Calculate checksum for the currently selected slot
+        private void calcChecksum(int slotNumber)
+        {
+            int saveChecksum = 0;
+
+            for (int i = 0; i < 1420; i++)
+            {
+                saveChecksum += saveData[0x280 + (slotNumber * 0x600) + i];
+            }
+
+            //Store checksum in little-endian byte order
+            saveData[0x80C + (slotNumber * 0x600)] = (byte)(saveChecksum & 0xFF);
+            saveData[0x80D + (slotNumber * 0x600)] = (byte)((saveChecksum & 0xFF00) >> 8);
+            saveData[0x80E + (slotNumber * 0x600)] = (byte)((saveChecksum & 0xFF0000) >> 16);
+            saveData[0x80F + (slotNumber * 0x600)] = (byte)((saveChecksum & 0xFF000000) >> 24);
+        }
+
+        private void cancelButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void okButton_Click(object sender, EventArgs e)
+        {
+            //Update save data
+            updateData(currentSaveSlot);
+
+            //Edited data should be return after pressing OK button
+            confirmedFlag = true;
+            this.Close();
+        }
+
+        private void mainWindow_Paint(object sender, PaintEventArgs e)
+        {
+            //Draw gray rectangle
+            e.Graphics.FillRectangle(new SolidBrush(Color.FromArgb(80, 80, 80)), 0, 0, this.Width, 30);
+        }
+    }
+}
